48111a8d2f01e03ac397c3ba8345f01a792a7d0d
Bryan Apellanes <bryan.apellanes@okta.com>
2020-04-28 10:36:04 -0700

    basic Okta integration for .Net Core Mvc and RazorPages projects

---
+++ Bam.Okta.RazorPages/Pages/SignIn.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Okta.AspNetCore;
+
+namespace Bam.Okta.RazorPages.Pages
+{
+    public class SignInModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Challenge(OktaDefaults.MvcAuthenticationScheme);
+            }
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ Bam.Okta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bam.Okta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
